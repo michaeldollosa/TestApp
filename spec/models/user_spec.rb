48dc66a1a943b91c3cf04675155bda99d4dde493
michael.dollosa <michael.dollosa@yahoo.com>
2013-01-15 14:52:28 +0800

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,7 +20,7 @@ describe User do
 
   subject { @user }
 
-  it { should respond_to (:authenticate) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
